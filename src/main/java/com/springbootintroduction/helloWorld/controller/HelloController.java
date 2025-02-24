04fe587f5bb6e83d2ca2c73852d8af69b3eb9643
PRINCE RAJ <rajprince031@gmail.com>
2025-02-24 13:00:22 +0530

    [Prince Raj] add . uc2 Log Messages

--- src/main/java/com/springbootintroduction/helloWorld/controller/HelloController.java
+++ src/main/java/com/springbootintroduction/helloWorld/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.springbootintroduction.helloWorld.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+    public static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String hello(){
+        logger.info("this is hello page");
         return "{ \"message\" : \"Hello from BridgeLaz\"}";
     }
 
--- src/main/java/com/springbootintroduction/helloWorld/controller/HelloWebController.java
+++ src/main/java/com/springbootintroduction/helloWorld/controller/HelloWebController.java
@@ -1,13 +1,19 @@
 package com.springbootintroduction.helloWorld.controller;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HelloWebController {
+
+    public static final Logger logger = LoggerFactory.getLogger(HelloWebController.class);
     @GetMapping("/web")
     public String hello(Model model){
+        logger.info("I am web Page");
         model.addAttribute("message" , "Hello from BridgeLabz");
         return "hello";
 
